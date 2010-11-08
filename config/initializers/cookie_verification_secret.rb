88d4a009642a7ae1e0e0320b8f6ef242eb953b78
fabbadger <fabbadgerSF@gmail.com>
2010-11-08 12:51:14 +0100

    Initial Commit for simple_adserver

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'a83748e2a760418312e4a02d3e8677fd0c1ef3cad158152a4b43348c6f34f7b4942aa304b76cf847fde4e944c9bae3e168736827eda0478b944cd3fdb288ca10';
