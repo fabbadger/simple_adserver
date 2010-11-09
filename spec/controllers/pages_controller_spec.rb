a3fd1bec24d43766d0228e028279ecd088aadc75
fabbadger <fabbadgerSF@gmail.com>
2010-11-09 04:21:02 +0100

    generated spec dir

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe PagesController do
+
+  #Delete these examples and add some real ones
+  it "should use PagesController" do
+    controller.should be_an_instance_of(PagesController)
+  end
+
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'admin'" do
+    it "should be successful" do
+      get 'admin'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'report'" do
+    it "should be successful" do
+      get 'report'
+      response.should be_success
+    end
+  end
+end
